0effa874a0f678293c5e76883f3209b28d4bb96a
kevwhitt-hee <kevin.whittaker@hee.nhs.uk>
2024-02-07 08:55:21 +0000

    TD-3676 Updates OpenAPI branding and links following move to NHS England

--- OpenAPI/LearningHub.Nhs.OpenApi/Configuration/ConfigurationExtensions.cs
+++ OpenAPI/LearningHub.Nhs.OpenApi/Configuration/ConfigurationExtensions.cs
@@ -1,5 +1,5 @@
-// <copyright file="ConfigurationExtensions.cs" company="HEE.nhs.uk">
-// Copyright (c) HEE.nhs.uk.
+// <copyright file="ConfigurationExtensions.cs" company="NHS England">
+// Copyright (c) NHS England.
 // </copyright>
 
 namespace LearningHub.NHS.OpenAPI.Configuration
--- OpenAPI/LearningHub.Nhs.OpenApi/Program.cs
+++ OpenAPI/LearningHub.Nhs.OpenApi/Program.cs
@@ -1,5 +1,5 @@
-// <copyright file="Program.cs" company="HEE.nhs.uk">
-// Copyright (c) HEE.nhs.uk.
+// <copyright file="Program.cs" company="NHS England">
+// Copyright (c) NHS England.
 // </copyright>
 
 namespace LearningHub.NHS.OpenAPI

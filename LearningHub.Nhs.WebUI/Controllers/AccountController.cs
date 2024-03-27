03e82ee205e987cad7d823b376e76a94ea25c565
Arunima George <arunima.george@nhs.net>
2024-03-27 16:24:51 +0000

    TD-3953: Manager Role fix

--- LearningHub.Nhs.WebUI/Controllers/AccountController.cs
+++ LearningHub.Nhs.WebUI/Controllers/AccountController.cs
@@ -1267,15 +1267,10 @@
             var region = await this.regionService.GetAllAsync();
             var specialty = await this.specialtyService.GetSpecialtiesAsync();
 
-            // var role = await this.jobRoleService.GetFilteredAsync(accountCreationViewModel.CurrentRoleName);
-            // if (role.Count > 0)
-            // {
-            //    accountCreationViewModel.CurrentRoleName = role.FirstOrDefault(x => x.Id == int.Parse(accountCreationViewModel.CurrentRole)).NameWithStaffGroup;
-            // }
-            var role = await this.jobRoleService.GetPagedFilteredAsync(accountCreationViewModel.CurrentRoleName, accountCreationViewModel.CurrentPageIndex, UserRegistrationContentPageSize);
-            if (role.Item1 > 0)
+            var role = await this.jobRoleService.GetFilteredAsync(accountCreationViewModel.CurrentRoleName);
+            if (role.Count > 0)
             {
-                accountCreationViewModel.CurrentRoleName = role.Item2.FirstOrDefault(x => x.Id == int.Parse(accountCreationViewModel.CurrentRole)).NameWithStaffGroup;
+                accountCreationViewModel.CurrentRoleName = role.FirstOrDefault(x => x.Id == int.Parse(accountCreationViewModel.CurrentRole)).NameWithStaffGroup;
             }
 
             var grade = await this.gradeService.GetGradesForJobRoleAsync(int.TryParse(accountCreationViewModel.CurrentRole, out int roleId) ? roleId : 0);

656aca12db672b8b7c013159d59c1a318da83f33
binon <Binon.Yesudhas@hee.nhs.uk>
2024-04-05 11:30:25 +0100

    Got the latest and removed copy right statements

--- AdminUI/LearningHub.Nhs.AdminUI/Helpers/FeatureFlags.cs
+++ AdminUI/LearningHub.Nhs.AdminUI/Helpers/FeatureFlags.cs
@@ -1,8 +1,4 @@
-﻿// <copyright file="FeatureFlags.cs" company="HEE.nhs.uk">
-// Copyright (c) HEE.nhs.uk.
-// </copyright>
-
-namespace LearningHub.Nhs.AdminUI.Helpers
+﻿namespace LearningHub.Nhs.AdminUI.Helpers
 {
     /// <summary>
     /// <see cref="FeatureFlags"/>.
--- LearningHub.Nhs.WebUI/Helpers/FeatureFlags.cs
+++ LearningHub.Nhs.WebUI/Helpers/FeatureFlags.cs
@@ -1,8 +1,4 @@
-﻿// <copyright file="FeatureFlags.cs" company="HEE.nhs.uk">
-// Copyright (c) HEE.nhs.uk.
-// </copyright>
-
-namespace LearningHub.Nhs.WebUI.Helpers
+﻿namespace LearningHub.Nhs.WebUI.Helpers
 {
     /// <summary>
     /// Defines the <see cref="FeatureFlags" />.

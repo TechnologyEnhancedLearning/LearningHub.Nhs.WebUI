fa11301968a8e328477edba9256da0d23a08cc83
binon <Binon.Yesudhas@hee.nhs.uk>
2024-04-18 20:18:06 +0100

    TD3828 - Configured Case and assessment to use AMS flag

--- LearningHub.Nhs.WebUI/Scripts/vuesrc/contribute-resource/ContributeCaseOrAssessmentContent.vue
+++ LearningHub.Nhs.WebUI/Scripts/vuesrc/contribute-resource/ContributeCaseOrAssessmentContent.vue
@@ -2,17 +2,17 @@
     <div class="contribute-case-component lh-padding-fluid">
         <div class="lh-container-xl py-15">
             <div class="py-10 text-center placeholder-text"
-                 v-if="!hasContentOnPage">You have not added any content to this page yet
+                 v-if="!hasContentOnPage">
+                You have not added any content to this page yet
             </div>
             <template v-else>
-                <FilteredBlockCollectionView
-                    :resourceType="resourceType"
-                    :blockCollection="blockCollection"
-                    :selection="blockCollection => blockCollection.getBlocksByPage(page).filter(block => block.blockType !== BlockTypeEnum.Question)"
-                    :can-be-duplicated="duplicatingBlocks"
-                    :blocksToDuplicate="blocksToDuplicate"
-                    @annotateWholeSlideImage="showSlideWithAnnotations"
-                    @duplicateBlock="blockId => $emit('duplicateBlock', blockId)"/>
+                <FilteredBlockCollectionView :resourceType="resourceType"
+                                             :blockCollection="blockCollection"
+                                             :selection="blockCollection => blockCollection.getBlocksByPage(page).filter(block => block.blockType !== BlockTypeEnum.Question)"
+                                             :can-be-duplicated="duplicatingBlocks"
+                                             :blocksToDuplicate="blocksToDuplicate"
+                                             @annotateWholeSlideImage="showSlideWithAnnotations"
+                                             @duplicateBlock="blockId => $emit('duplicateBlock', blockId)" />
             </template>
 
             <ContributeAddContentBlock @add="choosingNewContentBlock = true"></ContributeAddContentBlock>
@@ -25,13 +25,32 @@
                    ref="addMediaInput"
                    multiple
                    @change="uploadNewMediaFiles"
-                   class="visually-hidden"/>
+                   class="visually-hidden" />
+
+            <div v-if="avUnavailableMessage">
+                <transition name="modal">
+                    <div class="modal-mask">
+                        <div class="modal-wrapper">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div v-html="audioVideoUnavailableView"></div>
+                                    <div class="modal-footer modal-footer--buttons">
+                                        <button type="button" class="nhsuk-button nhsuk-button--secondary" @click="cancelAVUnavailModal">Cancel</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </transition>
+            </div>
+
         </div>
     </div>
 </template>
 
 <script lang="ts">
     import Vue, { PropOptions } from 'vue';
+    import { resourceData } from '../data/resource';
     import ContributeBlock from './ContributeBlock.vue';
     import ContributeAddContentBlock from './ContributeAddContentBlock.vue';
     import ContributeChooseContentBlockType from './ContributeChooseContentBlockType.vue';
@@ -40,11 +59,13 @@
     import {
         FileUploadType,
         getAllowedFileExtensionsInAcceptFormat,
-        startUploadsFromFileElement
+        startUploadsFromFileElement,
+        getMediaTypeFromFileExtensionContributeCaseOrAssessment        
     } from '../helpers/fileUpload';
     import FilteredBlockCollectionView from './components/questions/FilteredBlockCollectionView.vue';
     import { WholeSlideImageModel } from "../models/contribute-resource/blocks/wholeSlideImageModel";
     import { ResourceType } from "../constants";
+    import { MediaTypeEnum } from '../models/contribute-resource/blocks/mediaTypeEnum';
 
     export default Vue.extend({
         props: {
@@ -64,11 +85,19 @@
             return {
                 choosingNewContentBlock: false,
                 BlockTypeEnum,
+                contributeResourceAVFlag: true,
+                avUnavailableMessage: false
             };
+        },
+        created() {
+            this.getContributeResAVResourceFlag();
         },
         computed: {
             hasContentOnPage(): boolean {
                 return this.blockCollection?.getBlocksByPage(this.page)?.filter(block => block.blockType !== BlockTypeEnum.Question).length > 0;
+            },
+            audioVideoUnavailableView(): string {
+                return this.$store.state.getAVUnavailableView;
             }
         },
         methods: {
@@ -90,14 +119,42 @@
                 }
             },
             async uploadNewMediaFiles(event: any): Promise<void> {
-                startUploadsFromFileElement(
-                    event.target as HTMLInputElement,
-                    (fileId, mediaType) => this.blockCollection.addMediaBlock(fileId, mediaType, this.page)
-                );
+                var targetItem = event.target as HTMLInputElement;
+                var startUpload = true;
+
+                if (targetItem.value !== '') {
+                    for (let i = 0; i < targetItem.files.length; i++) {
+                        const file = targetItem.files[i] as File;
+                        const fileExtension = file.name.split('.').pop();
+                        const mediaType = getMediaTypeFromFileExtensionContributeCaseOrAssessment(`.${fileExtension}`);
+
+                        if (!this.contributeResourceAVFlag && (mediaType === MediaTypeEnum.Audio || mediaType === MediaTypeEnum.Video)) {
+                            startUpload = false;
+                            this.avUnavailableMessage = true;
+                        }
+                        else { startUpload = true; }
+                    }
+                }
+
+                if (startUpload) {
+                    startUploadsFromFileElement(
+                        event.target as HTMLInputElement,
+                        (fileId, mediaType) => this.blockCollection.addMediaBlock(fileId, mediaType, this.page)
+                    );
+                }
+                targetItem.value = '';
             },
             showSlideWithAnnotations(wholeSlideImageToShow: WholeSlideImageModel) {
                 this.$emit('annotateWholeSlideImage', wholeSlideImageToShow, false);
             },
+            cancelAVUnavailModal() {
+                this.avUnavailableMessage = false;
+            },
+            getContributeResAVResourceFlag() {
+                resourceData.getContributeAVResourceFlag().then(response => {
+                    this.contributeResourceAVFlag = response;
+                });
+            }
         },
     });
 </script>
@@ -113,5 +170,9 @@
 
     .placeholder-text {
         color: $nhsuk-grey;
+    }
+    .modal-content--scroll {
+        max-height: 90vh;
+        overflow-y: auto;
     }
 </style>

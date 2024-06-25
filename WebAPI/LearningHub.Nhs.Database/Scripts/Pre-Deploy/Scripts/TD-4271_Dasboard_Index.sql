ccfd1e8d805b0dd70b642d9c197f1a815ec79cc4
Oluwatobi Awe <Oluwatobi.Awe@hee.nhs.uk>
2024-06-25 15:32:39 +0100

    TD-4271 dashboard service optimisation.

---
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Pre-Deploy/Scripts/TD-4271_Dasboard_Index.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE INDEX IX_Resource_CurrentResourceVersionId ON resources.Resource (CurrentResourceVersionId)
+GO
+------------------------------
+CREATE INDEX IX_Resource_ResourceTypeId ON resources.Resource (ResourceTypeId)
+GO
+------------------------------
+CREATE INDEX IX_ResourceVersion_ResourceId ON resources.ResourceVersion (ResourceId)
+GO
+-----------------------------
+CREATE INDEX IX_ResourceActivity_UserId_ActivityStatusId ON activity.ResourceActivity (UserId, ActivityStatusId)
+GO
+-------------------------------
+CREATE INDEX IX_NodePath_CatalogueNodeId_Deleted ON hierarchy.NodePath (CatalogueNodeId, Deleted)
+GO
+-------------------------------
+CREATE INDEX IX_UserBookmark_UserId_ResourceReferenceId_Deleted ON hub.UserBookmark (UserId, ResourceReferenceId, Deleted)
+GO

a18bdda4fdbee5048fc5218c46455ba18b554f4d
Oluwatobi Awe <Oluwatobi.Awe@hee.nhs.uk>
2024-06-27 09:06:01 +0100

    TD-4270 Archive Log Db

---
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Scripts/TD-4270_Archive_LogDb.sql
@@ -0,0 +1,21 @@
+﻿-- Define the date 6 months ago
+DECLARE @SixMonthsAgo DATE = DATEADD(MONTH, -6, GETDATE());
+
+
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'LogArchive' AND schema_id = SCHEMA_ID('hub'))
+BEGIN
+    SELECT TOP (0) *
+    INTO hub.LogArchive
+    FROM hub.Log;
+END
+
+
+INSERT INTO hub.LogArchive ([Application], [Logged], [Level], [Message], [Logger], [Callsite], [Exception], [UserId], [Username])
+SELECT [Application], [Logged], [Level], [Message], [Logger], [Callsite], [Exception], [UserId], [Username]
+FROM hub.Log
+WHERE [Logged] < @SixMonthsAgo;
+
+
+DELETE FROM hub.Log WHERE [Logged] < @SixMonthsAgo;
+
+

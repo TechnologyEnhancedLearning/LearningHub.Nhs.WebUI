fe265455fb842bca2df8b5ddf793503f1ef4c6c4
Oluwatobi Awe <Oluwatobi.Awe@hee.nhs.uk>
2024-07-17 11:14:57 +0100

    archive script update

--- WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Scripts/TD-4270_Archive_LogDb.sql
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Scripts/TD-4270_Archive_LogDb.sql
@@ -1,8 +1,4 @@
-﻿-- Define the date 6 months ago
-DECLARE @SixMonthsAgo DATE;
-SET @SixMonthsAgo = DATEADD(MONTH, -6, GETDATE());
-GO
-
+﻿
 
 IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'LogArchive' AND schema_id = SCHEMA_ID('hub'))
 BEGIN
@@ -12,15 +8,15 @@ BEGIN
 END
 GO
 
-
+DECLARE @SixMonthsAgo DATE;
+SET @SixMonthsAgo = DATEADD(MONTH, -6, GETDATE());
 INSERT INTO hub.LogArchive ([Application], [Logged], [Level], [Message], [Logger], [Callsite], [Exception], [UserId], [Username])
 SELECT [Application], [Logged], [Level], [Message], [Logger], [Callsite], [Exception], [UserId], [Username]
 FROM hub.Log
 WHERE [Logged] < @SixMonthsAgo;
 GO
 
-
+DECLARE @SixMonthsAgo DATE;
+SET @SixMonthsAgo = DATEADD(MONTH, -6, GETDATE());
 DELETE FROM hub.Log WHERE [Logged] < @SixMonthsAgo;
 GO
-
-

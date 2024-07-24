24132d654f43d2ad109351789f7aaf4171d4e168
Swapnamol Abraham <Swapnamol.Abraham@hee.nhs.uk>
2024-07-24 18:18:41 +0100

    Removed the commented lines

--- LearningHub.Nhs.WebUI/Scripts/vuesrc/contribute-resource/components/KeyWordsEditor.vue
+++ LearningHub.Nhs.WebUI/Scripts/vuesrc/contribute-resource/components/KeyWordsEditor.vue
@@ -100,7 +100,6 @@
 
                             }
                             else {
-                                ////this.keywordError = true;
                                 this.keywordLengthExceeded = true;
                             }
                         }

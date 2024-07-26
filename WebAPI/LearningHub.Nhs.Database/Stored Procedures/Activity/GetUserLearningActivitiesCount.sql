e28850ef87318091091c0f4d80a256bab1f2bea3
ArunimaGeorge <163844873+ArunimaGeorge@users.noreply.github.com>
2024-07-26 17:29:07 +0100

    Update GetUserLearningActivitiesCount.sql

--- WebAPI/LearningHub.Nhs.Database/Stored Procedures/Activity/GetUserLearningActivitiesCount.sql
+++ WebAPI/LearningHub.Nhs.Database/Stored Procedures/Activity/GetUserLearningActivitiesCount.sql
@@ -215,6 +215,12 @@ FROM (
 													)
 										
 											)
+											--OR         
+										--(
+										--		([Res].[ResourceTypeId] IN (1,5,10,12) AND  [ResourceActivity].[ActivityStatusId] = 3)
+										--	  AND
+										--		([Res].[ResourceTypeId] NOT IN (2,7,8) AND  [ResourceActivity].[ActivityStatusId] = 3)
+										--)
 									)
 						)
 						OR	

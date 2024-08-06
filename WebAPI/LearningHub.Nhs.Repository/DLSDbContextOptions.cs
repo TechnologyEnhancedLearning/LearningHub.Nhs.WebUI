0bdd145c90876f52b7b24ef8454a067a762e6a94
Colin Beeby <colin.beeby@hee.nhs.uk>
2024-08-06 12:15:37 +0100

    Added DLS DeviceType controller API

---
+++ WebAPI/LearningHub.Nhs.Repository/DLSDbContextOptions.cs
@@ -0,0 +1,33 @@
+﻿namespace LearningHub.Nhs.Repository
+{
+    using System.Collections.Generic;
+    using LearningHub.Nhs.Repository.Map;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// DLSDbContextOptions class.
+    /// </summary>
+    public class DLSDbContextOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DLSDbContextOptions"/> class.
+        /// </summary>
+        /// <param name="options">The options.</param>
+        /// <param name="mappings">The mappings.</param>
+        public DLSDbContextOptions(DbContextOptions<DLSDbContext> options, IEnumerable<IEntityTypeMap> mappings)
+        {
+            this.Options = options;
+            this.Mappings = mappings;
+        }
+
+        /// <summary>
+        /// Gets the options.
+        /// </summary>
+        public DbContextOptions<DLSDbContext> Options { get; }
+
+        /// <summary>
+        /// Gets the mappings.
+        /// </summary>
+        public IEnumerable<IEntityTypeMap> Mappings { get; }
+    }
+}
---
+++ WebAPI/LearningHub.Nhs.Repository/DLSEntities/DeviceTypes.cs
@@ -0,0 +1,21 @@
+﻿namespace LearningHub.Nhs.Api.DLSEntities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The DLSDeviceType class.
+    /// </summary>
+    public class DeviceTypes
+    {
+        /// <summary>
+        /// Gets or sets the id for the DLSDeviceType.
+        /// </summary>
+        [Key]
+        public int DeviceTypeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the deviceType for the DLSDeviceType.
+        /// </summary>
+        public string DeviceType { get; set; }
+    }
+}

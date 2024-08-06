3459a2110dcd7cbe67b9c9976aa087b0b2470a13
Colin Beeby <colin.beeby@hee.nhs.uk>
2024-08-06 12:15:37 +0100

    Added DLS DeviceType controller API

---
+++ WebAPI/LearningHub.Nhs.API/Controllers/DLSDeviceTypeController.cs
@@ -0,0 +1,123 @@
+﻿namespace LearningHub.Nhs.Api.Controllers
+{
+    using LearningHub.Nhs.Api.DLSEntities;
+    using LearningHub.Nhs.Repository;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// The DLS Device Types Controller.
+    /// </summary>
+    public class DLSDeviceTypeController : Controller
+    {
+        private readonly DLSDbContext dlsDbContext;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DLSDeviceTypeController"/> class.
+        /// </summary>
+        /// <param name="context">The DLS DB context.</param>
+        public DLSDeviceTypeController(DLSDbContext context)
+        {
+            this.dlsDbContext = context;
+        }
+
+        /// <summary>
+        /// GET Details.
+        /// </summary>
+        /// <param name="id">The Id.</param>
+        /// <returns>The <see cref="ActionResult"/>.</returns>
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var deviceTypes = this.dlsDbContext.DeviceTypes.Find(id);
+
+                return this.Ok(deviceTypes);
+            }
+            catch
+            {
+                return this.BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// POST Create a new DLSDeviceType.
+        /// </summary>
+        /// <param name="deviceType">The device type.</param>
+        /// <returns>The <see cref="ActionResult"/>.</returns>
+        [HttpPost]
+        public ActionResult Create(string deviceType)
+        {
+            try
+            {
+                var deviceTypes = new DeviceTypes
+                {
+                    DeviceType = deviceType,
+                };
+                this.dlsDbContext.DeviceTypes.Add(deviceTypes);
+
+                this.dlsDbContext.SaveChanges();
+
+                return this.Ok();
+            }
+            catch
+            {
+                return this.BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// POST Edit an existing DLSDeviceType.
+        /// </summary>
+        /// <param name = "id" > The id of the device type.</param>
+        /// <param name = "deviceType" > The device type.</param>
+        /// <returns>The<see cref="ActionResult"/>.</returns>
+        [HttpPut]
+        public ActionResult Edit(int id, string deviceType)
+        {
+            try
+            {
+                var deviceTypes = new DeviceTypes
+                {
+                    DeviceTypeId = id,
+                    DeviceType = deviceType,
+                };
+                this.dlsDbContext.DeviceTypes.Update(deviceTypes);
+
+                this.dlsDbContext.SaveChanges();
+
+                return this.Ok();
+            }
+            catch
+            {
+                return this.BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// DELETE an existing DLS DeviceType.
+        /// </summary>
+        /// <param name="id">The id of the device type.</param>
+        /// <returns>The<see cref="ActionResult"/>.</returns>
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var deviceTypes = new DeviceTypes
+                {
+                    DeviceTypeId = id,
+                };
+                this.dlsDbContext.DeviceTypes.Remove(deviceTypes);
+
+                this.dlsDbContext.SaveChanges();
+
+                return this.Ok();
+            }
+            catch
+            {
+                return this.BadRequest();
+            }
+        }
+    }
+}
---
+++ WebAPI/LearningHub.Nhs.Repository/DLSDbContext.cs
@@ -0,0 +1,31 @@
+﻿namespace LearningHub.Nhs.Repository
+{
+    using LearningHub.Nhs.Api.DLSEntities;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// The DLS db context.
+    /// </summary>
+    public class DLSDbContext : DbContext
+    {
+        /// <summary>
+        /// The options..
+        /// </summary>
+        private readonly DbContextOptions<DLSDbContext> options;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DLSDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The options<see cref="DbContextOptions"/>.</param>
+        public DLSDbContext(DbContextOptions<DLSDbContext> options)
+            : base(options)
+        {
+            this.options = options;
+        }
+
+        /// <summary>
+        /// Gets or sets the dataset for the DLS device type.
+        /// </summary>
+        public DbSet<DeviceTypes> DeviceTypes { get; set; }
+    }
+}

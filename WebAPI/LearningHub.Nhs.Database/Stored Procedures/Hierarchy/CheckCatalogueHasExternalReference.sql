05bff2ba8a88752031d56cfb818b9b2d23d3c298
Sarathlal Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2024-09-03 16:28:51 +0100

    TD-4336: Scenario when a single resource is added as external reference is added issue handled

--- WebAPI/LearningHub.Nhs.Database/Stored Procedures/Hierarchy/CheckCatalogueHasExternalReference.sql
+++ WebAPI/LearningHub.Nhs.Database/Stored Procedures/Hierarchy/CheckCatalogueHasExternalReference.sql
@@ -17,7 +17,7 @@ AS
 
 BEGIN
 		;WITH
-		  cteExternalReference(NodeId,  ParentNodeId, PrimaryCatalogueNodeId, InitialNodePath)
+		  cteNode(NodeId,  ParentNodeId, PrimaryCatalogueNodeId, InitialNodePath)
 		  AS
 		  (
 			SELECT 
@@ -50,7 +50,7 @@ BEGIN
 			INNER JOIN
 				hierarchy.[Node] n ON nl.ChildNodeId = n.Id
 			INNER JOIN	
-				cteExternalReference cte ON nl.ParentNodeId = cte.NodeId
+				cteNode cte ON nl.ParentNodeId = cte.NodeId
             INNER JOIN
                 hierarchy.NodeVersion nv ON n.CurrentNodeVersionId = nv.Id
 			WHERE 
@@ -59,10 +59,47 @@ BEGIN
                 AND n.Deleted = 0
                 AND nl.Deleted = 0
 
-			)
+			),
 		
-		SELECT @HasExternalCatalogueReference=case when  count(distinct cte.PrimaryCatalogueNodeId ) > 1 then 1 ELSE 0 END 
-		FROM
-			cteExternalReference cte
+		 cteResource
+		 AS (
+				SELECT cte.PrimaryCatalogueNodeId 
+							FROM
+								cteNode cte
+				UNION 
+
+				SELECT 
+					rv.PrimaryCatalogueNodeId AS PrimaryCatalogueNodeId
+				FROM
+					hierarchy.NodePath np
+				INNER JOIN
+					hierarchy.NodeResource nr ON np.NodeId = nr.NodeId
+				INNER JOIN
+					resources.[Resource] r ON nr.ResourceId = r.Id
+				INNER JOIN
+					resources.ResourceVersion rv ON rv.resourceId = nr.ResourceId
+				LEFT JOIN
+					resources.ResourceReference rr ON rr.ResourceId = nr.ResourceId AND rr.NodePathId = np.Id AND rr.Deleted = 0
+				LEFT JOIN
+					resources.VideoResourceVersion vrv ON vrv.ResourceVersionId = rv.Id AND vrv.Deleted = 0
+				LEFT JOIN 
+					resources.AudioResourceVersion arv ON arv.ResourceVersionId = rv.Id AND arv.Deleted = 0
+				LEFT JOIN
+					resources.ResourceVersionEvent rve ON rve.ResourceVersionId = rv.Id AND rve.ResourceVersionEventTypeId = 6 /* Unpublished by admin */
+				LEFT JOIN 
+					resources.ResourceVersion rvd ON r.Id = rvd.ResourceId AND rvd.Id > rv.Id AND rvd.Deleted = 0
+				WHERE 
+					np.CatalogueNodeId = @CatalogueNodeId
+					 and rv.PrimaryCatalogueNodeId <>np.CatalogueNodeId
+					AND r.CurrentResourceVersionId IS not NULL
+					AND nr.VersionStatusId=2
+					AND nr.Deleted = 0
+					AND r.Deleted = 0
+					AND rv.Deleted = 0
+			)
+	SELECT @HasExternalCatalogueReference=case when  count(distinct cte.PrimaryCatalogueNodeId ) > 1 then 1 ELSE 0 END 
+
+	FROM
+			cteResource cte
 END
 GO

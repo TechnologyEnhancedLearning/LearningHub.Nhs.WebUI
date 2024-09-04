df60faae945645c2d232e5cabb93612c81a42862
Sarathlal Sarangadharan <Sarathlal.Sarangadharan@hee.nhs.uk>
2024-09-04 09:08:05 +0100

    TD-4336: Unused tables has been removed.

--- WebAPI/LearningHub.Nhs.Database/Stored Procedures/Hierarchy/CheckCatalogueHasExternalReference.sql
+++ WebAPI/LearningHub.Nhs.Database/Stored Procedures/Hierarchy/CheckCatalogueHasExternalReference.sql
@@ -68,8 +68,8 @@ BEGIN
 								cteNode cte
 				UNION 
 
-				SELECT 
-					rv.PrimaryCatalogueNodeId AS PrimaryCatalogueNodeId
+				SELECT 		
+					rv.PrimaryCatalogueNodeId AS PrimaryCatalogueNodeId			
 				FROM
 					hierarchy.NodePath np
 				INNER JOIN
@@ -78,16 +78,9 @@ BEGIN
 					resources.[Resource] r ON nr.ResourceId = r.Id
 				INNER JOIN
 					resources.ResourceVersion rv ON rv.resourceId = nr.ResourceId
-				LEFT JOIN
+				INNER JOIN
 					resources.ResourceReference rr ON rr.ResourceId = nr.ResourceId AND rr.NodePathId = np.Id AND rr.Deleted = 0
-				LEFT JOIN
-					resources.VideoResourceVersion vrv ON vrv.ResourceVersionId = rv.Id AND vrv.Deleted = 0
-				LEFT JOIN 
-					resources.AudioResourceVersion arv ON arv.ResourceVersionId = rv.Id AND arv.Deleted = 0
-				LEFT JOIN
-					resources.ResourceVersionEvent rve ON rve.ResourceVersionId = rv.Id AND rve.ResourceVersionEventTypeId = 6 /* Unpublished by admin */
-				LEFT JOIN 
-					resources.ResourceVersion rvd ON r.Id = rvd.ResourceId AND rvd.Id > rv.Id AND rvd.Deleted = 0
+				
 				WHERE 
 					np.CatalogueNodeId = @CatalogueNodeId
 					 and rv.PrimaryCatalogueNodeId <>np.CatalogueNodeId

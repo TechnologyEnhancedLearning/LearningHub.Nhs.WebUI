83e78d2fa4813db0a770b79c5abbedb3339d8c54
Swapnamol Abraham <Swapnamol.Abraham@hee.nhs.uk>
2024-09-24 11:55:31 +0100

    TD-4385: Referenced folder not being added to all required locations

---
+++ WebAPI/LearningHub.Nhs.Database/Stored Procedures/Hierarchy/HierarchyEditCreateNodeReference.sql
@@ -0,0 +1,93 @@
+﻿-------------------------------------------------------------------------------
+-- Author       Swapnamol Abraham
+-- Created      18-09-2024
+-- Purpose      Create node resource reference within a Hierarchy Edit.
+--
+-- Modification History
+--
+-- 18-09-2024  SA	Initial Revision.
+-------------------------------------------------------------------------------
+CREATE PROCEDURE [hierarchy].[HierarchyEditCreateNodeReference]
+(
+	@HierarchyEditDetailId int,
+	@MoveToHierarchyEditDetailId int,
+	@UserId int,
+	@UserTimezoneOffset int = NULL
+)
+
+AS
+
+BEGIN
+
+	BEGIN TRY
+
+		BEGIN TRAN	
+
+		DECLARE @AmendDate datetimeoffset(7) = ISNULL(TODATETIMEOFFSET(DATEADD(mi, @UserTimezoneOffset, GETUTCDATE()), @UserTimezoneOffset), SYSDATETIMEOFFSET())
+
+		DECLARE @HierarchyEditId int
+		DECLARE @ResourceId int, @ParentNodeId INT
+		SELECT @HierarchyEditId = HierarchyEditId, @ResourceId = ResourceId,@ParentNodeId = ParentNodeId FROM [hierarchy].[HierarchyEditDetail] WHERE Id = @HierarchyEditDetailId
+		
+
+	-- creating a folder reference into a referenced folder should affect all instances of the referenced folder.
+
+		DECLARE @CurrentNodeId INT,
+                @ReferenceHierarchyEditDetailId INT
+
+        SELECT @CurrentNodeId = hed.NodeId
+        FROM [hierarchy].[HierarchyEditDetail] hed
+        WHERE hed.Id = @MoveToHierarchyEditDetailId 
+		      AND HierarchyEditId = @HierarchyEditId
+		
+        -- Declare the cursor
+
+		 DECLARE NodeCursor CURSOR FOR 
+                  SELECT Id AS ReferenceHierarchyEditDetailId
+                  FROM hierarchy.HierarchyEditDetail
+                                         WHERE NodeId = @CurrentNodeId 
+											   AND HierarchyEditId = @HierarchyEditId
+											   AND deleted = 0
+        -- Open the cursor
+        OPEN NodeCursor;
+
+        -- Fetch the first row from the cursor
+        FETCH NEXT FROM NodeCursor
+        INTO @ReferenceHierarchyEditDetailId;
+
+        -- Loop until no more rows are returned
+        WHILE @@FETCH_STATUS = 0
+        BEGIN
+            --Execute the script to add new folder references in all referenced instances
+            EXEC [hierarchy].[HierarchyEditReferenceNode] @HierarchyEditDetailId, @ReferenceHierarchyEditDetailId, @UserId,@UserTimezoneOffset
+
+            -- Fetch the next row from the cursor
+            FETCH NEXT FROM NodeCursor
+            INTO @ReferenceHierarchyEditDetailId;
+        END
+
+        -- Close and deallocate the cursor
+        CLOSE NodeCursor;
+        --DEALLOCATE NodeCursor;
+       
+		COMMIT
+
+	END TRY
+	BEGIN CATCH
+	    DECLARE @ErrorMessage NVARCHAR(4000);  
+		DECLARE @ErrorSeverity INT;  
+		DECLARE @ErrorState INT;  
+  
+		SELECT   
+			@ErrorMessage = ERROR_MESSAGE(),  
+			@ErrorSeverity = ERROR_SEVERITY(),  
+			@ErrorState = ERROR_STATE();  
+  
+		IF @@TRANCOUNT > 0  
+			ROLLBACK TRAN;  
+
+		RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);  
+
+	END CATCH
+END
+Go

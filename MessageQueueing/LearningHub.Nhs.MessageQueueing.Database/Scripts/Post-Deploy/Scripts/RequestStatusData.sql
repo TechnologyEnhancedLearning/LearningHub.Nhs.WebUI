9426f30e4a49f79ced1980db1029066c039cdd7e
Arunima George <arunima.george@nhs.net>
2025-07-15 11:28:44 +0100

    Added Not Exists condition in sql data queries.

--- MessageQueueing/LearningHub.Nhs.MessageQueueing.Database/Scripts/Post-Deploy/Scripts/RequestStatusData.sql
+++ MessageQueueing/LearningHub.Nhs.MessageQueueing.Database/Scripts/Post-Deploy/Scripts/RequestStatusData.sql
@@ -1,6 +1,14 @@
-﻿INSERT [dbo].[RequestStatus] ([Id], [RequestStatus]) VALUES (1, N'Pending')
-GO
+﻿IF NOT EXISTS(SELECT Id FROM [dbo].[RequestStatus] WHERE Id = 1)
+BEGIN
+INSERT [dbo].[RequestStatus] ([Id], [RequestStatus]) VALUES (1, N'Pending')
+END
+
+IF NOT EXISTS(SELECT Id FROM [dbo].[RequestStatus] WHERE Id = 2)
+BEGIN
 INSERT [dbo].[RequestStatus] ([Id], [RequestStatus]) VALUES (2, N'Sent')
-GO
+END
+
+IF NOT EXISTS(SELECT Id FROM [dbo].[RequestStatus] WHERE Id = 3)
+BEGIN
 INSERT [dbo].[RequestStatus] ([Id], [RequestStatus]) VALUES (3, N'Failed')
-GO
+END
--- MessageQueueing/LearningHub.Nhs.MessageQueueing.Database/Scripts/Post-Deploy/Scripts/RequestTypeData.sql
+++ MessageQueueing/LearningHub.Nhs.MessageQueueing.Database/Scripts/Post-Deploy/Scripts/RequestTypeData.sql
@@ -1,6 +1,14 @@
-﻿INSERT [dbo].[RequestType] ([Id], [RequestType]) VALUES (1, N'Email')
-GO
+﻿IF NOT EXISTS(SELECT Id FROM [dbo].[RequestType] WHERE Id = 1)
+BEGIN
+INSERT [dbo].[RequestType] ([Id], [RequestType]) VALUES (1, N'Email')
+END
+
+IF NOT EXISTS(SELECT Id FROM [dbo].[RequestType] WHERE Id = 2)
+BEGIN
 INSERT [dbo].[RequestType] ([Id], [RequestType]) VALUES (2, N'SMS')
-GO
+END
+
+IF NOT EXISTS(SELECT Id FROM [dbo].[RequestType] WHERE Id = 3)
+BEGIN
 INSERT [dbo].[RequestType] ([Id], [RequestType]) VALUES (3, N'SingleEmail')
-GO
+END

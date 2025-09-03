91078fe0762a6c111cafa9944c432c5c64b3547c
Phil-NHS <philip.tate@nhs.net>
2025-09-03 09:42:03 +0100

    TD-5930 Bumping down elfh models because doesnt in pipeline and dropping search for this branch means no longer require it

--- LearningHub.Nhs.WebUI/Services/DashboardService.cs
+++ LearningHub.Nhs.WebUI/Services/DashboardService.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Net.Http;
-    using System.Runtime.InteropServices.WindowsRuntime;
     using System.Text;
     using System.Threading.Tasks;
     using LearningHub.Nhs.Models.Dashboard;

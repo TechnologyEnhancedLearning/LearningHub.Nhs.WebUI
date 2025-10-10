d7baf36abd63333803cacb3f1783274f6de8e8ce
OluwatobiAwe <oluwatobi.awe@nhs.net>
2025-10-10 14:37:09 +0100

    TD-6159 database update

---
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Scripts/TD-6159-turn-on-cdc.sql
@@ -0,0 +1,28 @@
+﻿-- Re-enable CDC
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.change_data_capture_tables
+    WHERE source_object_id = OBJECT_ID(N'hub.User')
+)
+BEGIN
+    EXEC sys.sp_cdc_enable_table
+        @source_schema = N'hub',
+        @source_name   = N'User',
+        @role_name     = NULL,  
+        @supports_net_changes = 1;
+END
+GO
+
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.change_data_capture_tables
+    WHERE source_object_id = OBJECT_ID(N'hub.UserProfile')
+)
+BEGIN
+    EXEC sys.sp_cdc_enable_table
+        @source_schema = N'hub',
+        @source_name   = N'UserProfile',
+        @role_name     = NULL,
+        @supports_net_changes = 1;
+END
+GO

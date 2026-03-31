ea1aa216a82480fe18f9f0a3d28b8404d1b84451
Tobi Awe <oluwatobi.awe@nhs.net>
2026-04-01 00:29:36 +0100

    TD-7057 remove ELFH tables, SP and references

--- WebAPI/LearningHub.Nhs.Database/Tables/Elfh/IpCountryLookupTBL.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [elfh].[ipCountryLookupTBL](
-	[fromIP] [varchar](20) NOT NULL,
-	[toIP] [varchar](20) NOT NULL,
-	[country] [varchar](10) NOT NULL,
-	[fromInt] [bigint] NULL,
-	[toInt] [bigint] NULL
-) ON [PRIMARY]
-GO

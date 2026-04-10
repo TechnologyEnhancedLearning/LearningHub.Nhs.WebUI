c43b6a6248e190deb3ec1c4c5f1dcfe01ae17636
Binon <binon>
2026-04-10 09:25:39 +0100

    TD-7116, script to create new moodle instance config table and and script to insert default moodle prod instacne

---
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Scripts/TD-7116-mib_new_env.sql
@@ -0,0 +1,4 @@
+﻿  INSERT INTO MoodleInstanceConfigs 
+  (ShortName, BaseUrl, TokenSecretName, EnabledEndpoints, Weighting, IsEnabled, CreatedAt, UpdatedAt)
+VALUES 
+  ('moodle-prod', 'https://learn.learninghub.nhs.uk/', 'LearningHubMoodleClientSecretProd', 'users,courses,grades', 100, 1, GETUTCDATE(), GETUTCDATE());

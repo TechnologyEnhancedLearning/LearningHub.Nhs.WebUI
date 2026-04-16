1761847fc670cd3da4d0eef4c67db6cef38db1a0
Binon <binon>
2026-04-16 11:50:38 +0100

    Need to remosve some more dependency in openAPI project.

--- OpenAPI/LearningHub.Nhs.OpenApi.Services/Startup.cs
+++ OpenAPI/LearningHub.Nhs.OpenApi.Services/Startup.cs
@@ -7,7 +7,6 @@ namespace LearningHub.Nhs.OpenApi.Services
     using LearningHub.Nhs.OpenApi.Services.Interface.Services.Messaging;
     using LearningHub.Nhs.OpenApi.Services.Services;
     using LearningHub.Nhs.OpenApi.Services.Services.AzureSearch;
-    using LearningHub.Nhs.OpenApi.Services.Services.Findwise;
     using LearningHub.Nhs.OpenApi.Services.Services.Messaging;
     using LearningHub.Nhs.Services;
     using Microsoft.Extensions.Configuration;
@@ -93,16 +92,6 @@ namespace LearningHub.Nhs.OpenApi.Services
             services.AddScoped<IUserGroupService, UserGroupService>();
             services.AddScoped<IMoodleBridgeApiService, MoodleBridgeApiService>();
             services.AddScoped<IUserPasswordResetRequestsService, UserPasswordResetRequestsService>();
-
-            // Register IFindwiseApiFacade based on feature flag
-            if (useAzureSearch)
-            {
-                services.AddScoped<IFindwiseApiFacade, NullFindwiseApiFacade>();
-            }
-            else
-            {
-                services.AddScoped<IFindwiseApiFacade, FindwiseApiFacade>();
-            }
         }
     }
 }

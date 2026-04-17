d2a574028b46f9e73680af050d8347cb35eb77a3
Tobi Awe <oluwatobi.awe@nhs.net>
2026-04-17 10:09:41 +0100

    activate CT if off

---
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Pre-Deploy/Scripts/ActivateChangeTracking.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS (
+    SELECT 1
+    FROM sys.change_tracking_databases
+    WHERE database_id = DB_ID()
+)
+BEGIN
+    ALTER DATABASE CURRENT
+    SET CHANGE_TRACKING = ON
+    (CHANGE_RETENTION = 2 DAYS, AUTO_CLEANUP = ON);
+END

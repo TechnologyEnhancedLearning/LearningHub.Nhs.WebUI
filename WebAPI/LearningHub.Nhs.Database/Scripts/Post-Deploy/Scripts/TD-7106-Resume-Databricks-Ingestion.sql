011bbcb8053153ac8fd5b84642559eb073a34e22
Tobi Awe <oluwatobi.awe@nhs.net>
2026-04-17 14:30:13 +0100

    CT retention days update

--- WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Scripts/TD-7106-Resume-Databricks-Ingestion.sql
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Scripts/TD-7106-Resume-Databricks-Ingestion.sql
@@ -9,7 +9,7 @@ BEGIN
     EXEC dbo.lakeflowSetupChangeTracking
     @Tables = 'activity.ResourceActivity,resources.VersionStatus,resources.ResourceType,activity.ScormActivity,activity.ActivityStatus,activity.ScormActivityInteraction,activity.ScormActivityInteractionCorrectResponse,resources.VideoResourceVersion,hierarchy.CatalogueNodeVersionProvider,resources.ResourceVersionKeyword,hierarchy.CatalogueNodeVersion,activity.MediaResourceActivity,resources.ResourceVersionAuthor,resources.ResourceVersionEvent,hub.UserProvider,resources.ResourceAccessibility,hierarchy.NodePath,resources.ResourceVersionRating,resources.ResourceVersionEventType,hierarchy.NodeType,hub.User,hierarchy.NodePathNode,hierarchy.VersionStatus,hub.UserProfile,hierarchy.NodeVersion,hierarchy.Publication,hub.Attribute,hierarchy.NodeResource,hierarchy.NodeLink,resources.Resource,hub.RoleUserGroup,hierarchy.CatalogueNodeVersionKeyword,hub.UserGroup,hub.UserUserGroup,resources.WebLinkResourceVersion,resources.ResourceReference,hierarchy.FolderNodeVersion,hub.Role,hub.AttributeType,activity.AssessmentResourceActivity,hub.Scope,resources.AssessmentResourceVersion,hub.UserGroupAttribute,hierarchy.Node,resources.ResourceVersion',
     @User = 'Elfhadmin',
-    @Retention = '2 DAYS';
+    @Retention = '7 DAYS';
 END
 ELSE
 BEGIN

0177acf26b03b23a6166c495ed7d92c585a83850
Colin Beeby <colin.beeby1@nhs.net>
2026-09-09 16:12:26 +0100

    modified CREATE TABLE sql to include new AllowedAccessLevels column

--- WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Script.PostDeployment.sql
+++ WebAPI/LearningHub.Nhs.Database/Scripts/Post-Deploy/Script.PostDeployment.sql
@@ -90,3 +90,4 @@ UPDATE [resources].[ResourceVersion] SET CertificateEnabled = 0 WHERE VersionSta
 :r .\Scripts\TD-7106-Resume-Databricks-Ingestion.sql
 :r .\Scripts\TD-7078-supportingindexes.sql
 :r .\Scripts\TD-7078-PopulateReadModelHistoricUserActivities.sql
+
